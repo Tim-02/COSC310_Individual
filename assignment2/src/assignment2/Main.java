6cdb2928b4e40af8acbd4ed5193e994e486bacfd
Tim <mammadov_teymur@yahoo.com>
2021-03-21 19:13:31 -0700

    WordNet is setup in IntelliJ (requires WordNet/3.1 in Program Files)

--- assignment2/src/assignment2/Main.java
+++ assignment2/src/assignment2/Main.java
@@ -1,7 +1,29 @@
 package assignment2;
 
+import net.didion.jwnl.*;
+import net.didion.jwnl.data.IndexWord;
+import net.didion.jwnl.data.POS;
+import net.didion.jwnl.dictionary.Dictionary;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
 	public static void main(String[] args) {
+		try {
+			JWNL.initialize(new FileInputStream("config/file_properties.xml"));
+			Dictionary d = Dictionary.getInstance();
+			String m = "muscles";
+			IndexWord i = d.lookupIndexWord(POS.NOUN, m);
+
+			System.out.println("User input: " + m);
+			System.out.println(i);
+			System.out.println(i.getSense(i.getSenseCount()));
+			System.out.println(i.getSynsetOffsets());
+		}
+		catch(FileNotFoundException | JWNLException e){
+			System.out.println(e);
+		}
 		Window w = new Window(800, 800);
 	}
 }
